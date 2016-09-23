ce894df27de585b827722db9e27569687a6da7cc
hurace <1101037077@qq.com>
2016-09-23 17:40:29 +0800

    mkdir php

---
+++ php/list_dir.php
@@ -0,0 +1,14 @@
+<?php
+
+	function list_dir($start){
+		$contents = scandir($start);
+		foreach ($contents as $item) {
+			if (is_dir("$start/$item") && (substr($item,0,1) != '.')){
+				list_dir("$start/$item");
+			} else {
+				echo $item,"\n";
+			}
+		}
+	}
+
+	list_dir('.');
